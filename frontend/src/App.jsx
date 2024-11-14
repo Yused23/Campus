09207b8907dc1ddd29817955a4bfc0a6fdebab35
Yused23 <michasanchez5@gmail.com>
2024-11-13 22:24:33 -0500

    se crea los inputs y el boton de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
+import { Formulario } from './components/Formulario'
 import './App.css'
 
 function App() {
   
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <div>
+      <Formulario/>
+   </div>
   )
 }
 
